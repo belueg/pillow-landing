f989a8d464d3c1aaeb443739e71e48268e43784f
Belén <belg1294@gmail.com>
2020-11-26 09:04:48 +0100

    Added: Logic to render Pillows information

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="details">
     <h2>Details page!</h2>
+    {{ pillowDetails }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Details'
+  name: 'Details',
+  data() {
+    return {
+      descriptionID: this.$route.params.id
+    }
+  },
+  computed: {
+    pillowDetails() {
+      return this.$store.state.pillows.find(
+        pillow => pillow.id == this.descriptionID
+      )
+    }
+  }
 }
 </script>
 
