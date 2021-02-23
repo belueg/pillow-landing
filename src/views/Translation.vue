c45a2f27600694eca617aebdfe9000a43893565d
Belén <belg1294@gmail.com>
2021-02-23 12:35:13 +0100

    Added: First translation and pluralization

---
+++ src/views/Translation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Translation">
+    <SwitchLocale />
+    <h1>{{ $t('main.welcome', { company: 'Pillow' }) }}</h1>
+    <h3>{{ $tc('main.messages', 2, { count: 3 }) }}</h3>
+  </div>
+</template>
+
+<script>
+import SwitchLocale from '@/components/SwitchLocale'
+export default {
+  name: 'Translation',
+  components: {
+    SwitchLocale
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Translation {
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   pluginOptions: {
     i18n: {
       locale: 'en',
-      fallbackLocale: 'en',
+      fallbackLocale: 'es',
       localeDir: 'locales',
       enableInSFC: false
     }
